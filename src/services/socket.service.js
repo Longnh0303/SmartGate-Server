469aa8f50655d9d95b4b6b75fe139090b6a31dae
Nguyen Hai Long <nguyenhailongtlu@gmail.com>
2023-11-21 17:02:42 +0700

    socket service fix +1

--- src/services/socket.service.js
+++ src/services/socket.service.js
@@ -8,7 +8,7 @@ const initSocketHandler = (httpServer) => {
   try {
     io = socketIO(httpServer, {
       cors: corsOptions,
-      transports: ["websocket", "polling"],
+      transports: ["websocket"],
       allowEIO3: true,
     });
     logger.info("Socket.io initialized");
