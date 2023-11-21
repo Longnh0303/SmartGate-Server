e27cfa4984c419847e1f689c953f435c7ed64168
Nguyen Hai Long <nguyenhailongtlu@gmail.com>
2023-11-21 21:26:32 +0700

    fix gate service next time+9

--- src/services/gate.service.js
+++ src/services/gate.service.js
@@ -5,12 +5,11 @@ const ApiError = require("../utils/ApiError");
 const logger = require("../config/logger");
 const { sendMessageToRoom } = require("./socket.service");
 
-const emitDeviceStatus = (macAddress, type, message) => {
-  const dataMsg = { type, data: { message } };
-  sendMessageToRoom(`${macAddress}_status`, "gate_status", dataMsg);
-};
-
 const checkCardAndPayment = async (body) => {
+  const emitDeviceStatus = (macAddress, type, message) => {
+    const dataMsg = { type, data: { message } };
+    sendMessageToRoom(`${macAddress}_status`, "gate_status", dataMsg);
+  };
   const { cardId, macAddress } = body;
   const rfid = await rfidService.getRfidByCardId(cardId);
 
@@ -21,6 +20,12 @@ const checkCardAndPayment = async (body) => {
       "Thẻ không tồn tại trong hệ thống"
     );
   }
+  emitDeviceStatus(macAddress, "error", "Thẻ đã vào");
+  const dataMsg = {
+    type: "access",
+    data: { cardId: cardId, time: Date.now() },
+  };
+  sendMessageToRoom(`${macAddress}_status`, "access_status", dataMsg);
 
   let history = await History.findOne({ cardId, done: false });
   const isNewEntry = !history;
@@ -35,12 +40,6 @@ const checkCardAndPayment = async (body) => {
 };
 
 const createNewHistoryEntry = async (cardId, rfid, macAddress) => {
-  emitDeviceStatus(macAddress, "error", "Thẻ đã vào");
-  const dataMsg = {
-    type: "access",
-    data: { cardId: cardId, time: Date.now() },
-  };
-  sendMessageToRoom(`${macAddress}_status`, "access_status", dataMsg);
   return await History.create({
     cardId,
     time_check_in: Date.now(),
@@ -52,12 +51,6 @@ const createNewHistoryEntry = async (cardId, rfid, macAddress) => {
 };
 
 const updateHistoryEntry = async (cardId, history, rfid, macAddress) => {
-  emitDeviceStatus(macAddress, "error", "Thẻ đã ra");
-  const dataMsg = {
-    type: "exit",
-    data: { cardId: cardId, time: Date.now() },
-  };
-  sendMessageToRoom(`${macAddress}_status`, "exit_status", dataMsg);
   const cost = 3000; // VND
   const millisecondsInADay = 1000 * 60 * 60 * 24;
   const daysCheckedIn = Math.ceil(
